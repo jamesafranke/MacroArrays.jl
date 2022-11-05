a93d4990f8b32ee78d5c3c88521db929b60b681e
jamesafranke <jfranke@uchicago.edu>
2022-11-05 17:29:22 -0500

    update macros

--- src/MacroArrays.jl
+++ src/MacroArrays.jl
@@ -6,14 +6,21 @@ using Dates
 using DataFrames
 using Chain
 using UnicodePlots
+using MacroTools
 #using Dagger
 #using Unitful
 
 include(joinpath("constructors", "constructors.jl"))
 include(joinpath("functions", "load.jl"))
 include(joinpath("functions", "manipulate.jl"))
-export MacroArray, @zload, @nload, @Aselect, @Aselect!, @Asubset, @Asubset!,
-    @Atransform, @Atransform!, @Aby, @Aby!, @Aresample, @Aresample!
+
+export MacroArray,
+     @zload, @nload, 
+     @Aselect, @Aselect!, 
+     @Asubset, @Asubset!,
+     @Atransform, @Atransform!, 
+     @Aby, @Aby!, 
+     @Aresample, @Aresample!
 
 end
 
--- src/functions/manipulations.jl
+++ src/functions/manipulations.jl
@@ -1,7 +1,35 @@
 ## Array Manipulations
 
 """
-    @Aselect(filepath::String, key::key,)
+    @with allows vars and dims to be called as symbols
+
+"""
+
+function exec(df, p::Pair)
+    cols = first(p)
+    fun = last(p)
+    fun(map(c -> DataFramesMeta.getsinglecolumn(df, c), cols)...)
+end
+exec(df, s::Union{Symbol, AbstractString}) = df[!, s]
+
+getsinglecolumn(df, s::DataFrames.ColumnIndex) = df[!, s]
+getsinglecolumn(df, s) = throw(ArgumentError("Only indexing with Symbols, strings and integers " *
+    "is currently allowed with $DOLLAR"))
+
+function with_helper(d, body)
+    # Make body an expression to force the
+    # complicated method of fun_to_vec
+    # in the case of QuoteNode
+    t = fun_to_vec(Expr(:block, body); no_dest=true)
+    :(DataFramesMeta.exec($d, $t))
+end
+
+macro with(d, body)
+    esc(with_helper(d, body))
+end
+
+"""
+    @Aselect filepath::String key::key
 macro to select cetain vars from the dataset based on key
 # Examples
 ```julia-repl
@@ -10,23 +38,23 @@ A = @Aselect A :pr :tas
 ```
 """
 
-macro @Aselect(A::MacroArray, key::key, kwargs...)
+macro Aselect(A::MacroArray, key::key, kwargs...)
     new = A[]
     return new
 end 
 
 """
-    @Aselect!(filepath::String, key::key,)
+    @Aselect! filepath::String key::key
 in place operation, see @Aselect
 """
 
-macro @Aselect!
+macro Aselect!()
 
     return nothing
 end
 
 """
-    @Asubset(filepath::String, condition)
+    @Asubset filepath::String condition
 macro to select cetain vars from the dataset based on key
 # Examples
 ```julia-repl
@@ -36,38 +64,61 @@ A = @Asubset A :pr .>  0.001
 A = @Asubset A :month .== 12
 ```
 """
-macro @Asubset
+function subset_helper(x, args...)
+    x, exprs, outer_flags, kw = get_df_args_kwargs(x, args...; wrap_byrow = false)
+
+    t = (fun_to_vec(ex; no_dest=true, outer_flags=outer_flags) for ex in exprs)
+    quote
+        $subset($x, $(t...); (skipmissing = true,)..., $(kw...))
+    end
+end
+
+function where_helper(x, args...)
+    x, exprs, outer_flags, kw = get_df_args_kwargs(x, args...; wrap_byrow = false)
+    t = (fun_to_vec(ex; no_dest=true, outer_flags=outer_flags) for ex in exprs)
+    quote
+        $subset($x, $(t...); skipmissing=true, $(kw...))
+    end
+end
+
+
+macro Asubset(x, args...)
+    esc(subset_helper(x, args...))
+end
+
+function rsubset_helper(x, args...)
+    x, exprs, outer_flags, kw = get_df_args_kwargs(x, args...; wrap_byrow = true)
+
+    t = (fun_to_vec(ex; no_dest=true, outer_flags=outer_flags) for ex in exprs)
+    quote
+        $subset($x, $(t...); (skipmissing = true,)..., $(kw...))
+    end
+end
+
+
+macro Atransform()
 
 end 
 
-macro @Asubset!
+macro Atransform!()
 
     return nothing
 end
 
-macro @Atransform
+macro Aby()
 
 end 
 
-macro @Atransform!
+macro Aby!()
 
     return nothing
 end
 
-macro @Aby
+macro Aresample()
 
 end 
 
-macro @Aby!
-
-    return nothing
-end
-
-macro @Aresample
-
-end 
-
-macro @Aresample!
+macro Aresample!()
 
     return nothing
 end
